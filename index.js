cb54859549fe299aa112da2fa86efd03428416b7
Rowan Crawford <wombleton@gmail.com>
2013-10-20 21:12:59 +1300

    Build sending emails
    Add error handling and form validation

--- index.js
+++ index.js
@@ -5,6 +5,8 @@
 
 var app,
     express = require('express'),
+    expressValidator = require('express-validator'),
+    flash = require('express-flash'),
     routes = require('./routes'),
     http = require('http'),
     path = require('path');
@@ -18,6 +20,10 @@ app.set('view engine', 'jade');
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
+app.use(expressValidator());
+app.use(express.cookieParser('keyboard cat'));
+app.use(express.session({ cookie: { maxAge: 60000 }}));
+app.use(flash());
 app.use(express.methodOverride());
 app.use(app.router);
 app.use(require('less-middleware')({ src: __dirname + '/public' }));
@@ -30,7 +36,8 @@ if ('development' == app.get('env')) {
 
 app.get('/', routes.index);
 app.get('/about', routes.about);
-
+app.get('/start', routes.start);
+app.post('/start', routes.sendEmail);
 
 http.createServer(app).listen(app.get('port'), function(){
     console.log('Express server listening on port ' + app.get('port'));
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,24 @@
-
-/*
- * GET home page.
-*/
-
-exports.index = function(req, res){
+exports.index = function(req, res) {
     res.render('index', { title: 'Be Resilient' });
 };
 
-exports.about = function(req, res){
+exports.start = function(req, res) {
+    res.render('start', { title: 'Be Resilient' });
+};
+
+exports.sendEmail = function(req, res) {
+    var errors;
+
+    req.assert('email', 'Need an email address').isEmail();
+
+    errors = req.validationErrors();
+
+    if (errors) {
+        req.flash('errors', errors);
+        res.redirect('/start');
+    }
+};
+
+exports.about = function(req, res) {
     res.render('about', { title: 'About Be Resilient' });
 };
