474efbb16e68e652a6a4947ddbfd5f6434e07ffa
Rowan Crawford <wombleton@gmail.com>
2013-10-20 15:47:55 +1300

    In progress, refined home page

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,12 @@
 
 /*
  * GET home page.
- */
+*/
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+    res.render('index', { title: 'Be Resilient' });
+};
+
+exports.about = function(req, res){
+    res.render('about', { title: 'About Be Resilient' });
 };
