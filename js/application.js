2e3c1d7a0cded75d7813323813ee79cbc8bc88db
Rowan Crawford <wombleton@gmail.com>
2014-07-23 21:19:41 +1200

    Mel's shopping list of changes.

--- js/application.js
+++ js/application.js
@@ -1,6 +1,8 @@
-$(document).on('click', '[href^=tel],[href^=mailto]', function() {
-  var $this = $(this),
+$(document).ready(function() {
+  $('[href^=tel],[href^=mailto]').each(function() {
+    var $this = $(this),
       href = $this.attr('href');
 
-  $this.text(href.substring(href.indexOf(':') + 1));
+    $this.attr('data-href', href.substring(href.indexOf(':') + 1));
+  });
 });
